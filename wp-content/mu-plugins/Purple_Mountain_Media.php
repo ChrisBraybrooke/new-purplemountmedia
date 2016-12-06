813c365b4fe9f1f76e8f3dd1424ca56de474f4f3
Christian Braybrooke <christianbraybrooke@Christians-MacBook-Pro.local>
2016-12-06 11:15:37 +0000

    Added DB Backup MU Plugin

--- wp-content/mu-plugins/Purple_Mountain_Media.php
+++ wp-content/mu-plugins/Purple_Mountain_Media.php
@@ -1,3 +1,4 @@
 <?php // mu-plugins/load.php
 require WPMU_PLUGIN_DIR.'/devbuddy-twitter-feed/devbuddy-twitter-feed.php';
 require WPMU_PLUGIN_DIR.'/optimus/optimus.php';
+require WPMU_PLUGIN_DIR.'/wp-sync-db-master/wp-sync-db.php';
